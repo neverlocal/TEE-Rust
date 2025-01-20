6abe7c5625ea88c2edbd6d2de1d4ee26676a4b3e
Fabrizio Romano Genovese <fabrizio@20squares.xyz>
2025-01-20 10:42:10 +0100

    Added missing lib.

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       {
         devShells.default = with pkgs; mkShell {
           buildInputs = [
+            systemd
             openssl
             rust-bin.stable.latest.default
             (rust-bin.stable.latest.default.override {
