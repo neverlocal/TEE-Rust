29abfa47927dfef3f73f90560488e53fa22cf9f6
Fabrizio Romano Genovese <fabrizio@20squares.xyz>
2025-01-31 19:16:13 +0100

    We should have AES cryptography ready.

--- examples/tee_aes/esp32c6/src/main.rs
+++ examples/tee_aes/esp32c6/src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+// AES PASSWORD TO COMMUNICATE WITH THE TEE
+const SHARED_SECRET: &[u8] = "SUp4SeCp@sSw0rd".as_bytes();
+
 extern crate alloc; // no_std requires a custom allocator
 use alloc::vec;
 use alloc::vec::Vec; // Needed for buffer manipulation
@@ -17,8 +20,8 @@ use esp_backtrace as _;
 use esp_println as _;
 
 use esp_hal::{
-    // aes::Aes,        // AES ecnryption-decryption scheme
-    // aes::Mode,       // AES mode (128, 256, etc)
+    aes::Aes,  // AES ecnryption-decryption scheme
+    aes::Mode, // AES mode (128, 256, etc)
     clock::CpuClock,
     main,
     peripherals::TIMG0,
@@ -121,7 +124,8 @@ fn main() -> ! {
     //////////////////
     // CRYPTOGRAPHY //
     //////////////////
-    let mut sha = Sha::new(peripherals.SHA);
+    let mut sha: Sha<'_> = Sha::new(peripherals.SHA);
+    let mut aes: Aes<'_> = Aes::new(peripherals.AES);
 
     //////////////////////////////
     // Conjugate Coding Helpers //
@@ -168,7 +172,7 @@ fn main() -> ! {
 
     #[derive(Zeroize, ZeroizeOnDrop, Deserialize, Format)]
     pub struct ConjugateCodingMeasurePlaintext {
-        outcomes: Vec<u8>
+        outcomes: Vec<u8>,
     }
 
     impl ConjugateCodingMeasurePlaintext {
@@ -176,13 +180,13 @@ fn main() -> ! {
             #[derive(Deserialize)]
             struct HexPlainData {
                 #[serde(deserialize_with = "deserialize_vec_from_hex_string")]
-                outcomes: Vec<u8>
+                outcomes: Vec<u8>,
             }
 
             let hex_data: HexPlainData = serde_json::from_slice(json_vec)?;
 
             Ok(ConjugateCodingMeasurePlaintext {
-                outcomes: hex_data.outcomes
+                outcomes: hex_data.outcomes,
             })
         }
     }
@@ -232,38 +236,58 @@ fn main() -> ! {
                 let buffer = store_serial_buffer(&mut buffer, &mut usb_serial);
                 if (buffer.len() > 0) && buffer[buffer.len() - 1] == 04 {
                     println!("");
-                    println!("[ PREPARATION ] Information submitted. Validating...");
+                    println!("[ PREPARATION ] Information submitted. Decripting...");
                     debug!("[ {:?} ] Buffer: {=[u8]:x}", PreparationInput, buffer);
                     buffer.pop();
                     debug!(
                         "[ {:?} ] Got rid of EOT char. New buffer: {=[u8]:x}",
                         PreparationInput, buffer
                     );
-                    match ConjugateCodingPreparePlaintext::deserialize(&buffer) {
+                    let mut decrypted_buffer =
+                        aes256(buffer, Mode::Decryption256, &mut aes, &mut sha);
+                    buffer.zeroize();
+                    debug!(
+                        "[ {:?} ] Buffer has been zeroized: New buffer: {=[u8]:x}",
+                        PreparationInput, buffer
+                    );
+                    debug!(
+                        "[ {:?} ] stripping padding 0s from decrypted_buffer",
+                        PreparationInput
+                    );
+                    if decrypted_buffer.len() > 0 {
+                        while decrypted_buffer[decrypted_buffer.len() - 1] == 0 {
+                            decrypted_buffer.pop();
+                            if decrypted_buffer.len() == 0 {
+                                break;
+                            };
+                        }
+                    }
+                    debug!(
+                        "[ {:?} ] Stripping completed. New decrypted_buffer: {=[u8]:x}",
+                        PreparationInput, decrypted_buffer
+                    );
+                    println!("[ PREPARATION ] Information decrypted. Validating...");
+                    match ConjugateCodingPreparePlaintext::deserialize(&decrypted_buffer) {
                         Err(_) => {
                             error!("[ PREPARATION ] Protocol wasn't able to parse the string. Restarting protocol...");
-                            buffer.zeroize();
+                            decrypted_buffer.zeroize();
                             debug!(
-                                "[ {:?} ] Buffer has been zeroized: New buffer: {=[u8]:x}",
-                                PreparationInput, buffer
+                                "[ {:?} ] decrypted_buffer has been zeroized: New decrypted_buffer: {=[u8]:x}",
+                                PreparationInput, decrypted_buffer
                             );
                             state_machine = PreparationDialog;
                             dbg_state_transition(PreparationInput, PreparationDialog);
                         }
-                        Ok(deserialized_buffer) => {
+                        Ok(mut deserialized_buffer) => {
                             println!("[ PREPARATION ] JSON input is of the right format. Validating information...");
                             debug!(
                                 "[ {:?} ] deserialized_buffer: {:?}",
                                 PreparationInput, deserialized_buffer
                             );
-                            buffer.zeroize();
+                            decrypted_buffer.zeroize();
                             debug!(
-                                "[ {:?} ] Buffer has been zeroized: New buffer: {=[u8]:x}",
-                                PreparationInput, buffer
-                            );
-                            debug!(
-                                "[ {:?} ] checking that the preparation provides 0 secret bits",
-                                PreparationInput
+                                "[ {:?} ] decrypted_buffer has been zeroized: New decrypted_buffer: {=[u8]:x}",
+                                PreparationInput, decrypted_buffer
                             );
                             match ConjugateCodingPrepare::new_plaintext(
                                 0,
@@ -291,6 +315,11 @@ fn main() -> ! {
                                     dbg_state_transition(PreparationInput, ProgramDialog);
                                 }
                             }
+                            deserialized_buffer.zeroize();
+                            debug!(
+                            "[ {:?} ] deserialized_buffer has been zeroized: New decrypted_buffer: {:?}",
+                            PreparationInput, deserialized_buffer
+                        );
                         }
                     };
                     wdt = watchdog_feed(wdt, PreparationInput);
@@ -322,7 +351,7 @@ fn main() -> ! {
                     program_input = buffer.to_vec();
                     debug!("[ {:?} ] program_input assigned", ProgramInput);
                     println!("[ PROGRAM INPUT ] Computing program input hash");
-                    program_hash = hash256(buffer,&mut sha)[0..preparation.total_size].to_vec();
+                    program_hash = hash256(buffer, &mut sha)[0..preparation.total_size].to_vec();
                     buffer.zeroize();
                     debug!(
                         "[ {:?} ] Buffer has been zeroized: New buffer: {=[u8]:x}",
@@ -523,45 +552,67 @@ fn watchdog_feed(mut wdt: Wdt<TIMG0>, state: StateMachine) -> Wdt<TIMG0> {
     return wdt;
 }
 
-fn hash256(buffer: &[u8],sha: &mut Sha<'_>) -> [u8; 32] {
+fn hash256(buffer: &[u8], sha: &mut Sha<'_>) -> [u8; 32] {
     let mut hasher: esp_hal::sha::ShaDigest<'_, Sha256, &mut Sha<'_>> = sha.start::<Sha256>();
     let mut hash_buffer: &[u8] = buffer;
     trace!("[ hash256 ] hash_buffer initialized.");
-    trace!("[ hash256 ] hash_buffer: {:?}", hash_buffer);
+    trace!("[ hash256 ] hash_buffer: {=[u8]:x}", hash_buffer);
 
     while !hash_buffer.is_empty() {
         // All the HW Sha functions are infallible so unwrap is fine to use if
         // you use block!
         hash_buffer = block!(hasher.update(hash_buffer)).unwrap();
-        trace!("[ hash256 ] hash_buffer: {:?}", hash_buffer);
+        trace!("[ hash256 ] hash_buffer: {=[u8]:x}", hash_buffer);
     }
     let mut output = [0u8; 32];
     block!(hasher.finish(output.as_mut_slice())).unwrap();
-    trace!("[ hash256 ] hash: {:?}", output);
+    trace!("[ hash256 ] hash: {=[u8]:x}", output);
     return output;
+}
+
+fn aes256(buffer: &mut Vec<u8>, mode: Mode, aes: &mut Aes<'_>, sha: &mut Sha<'_>) -> Vec<u8> {
+    let keybuf = hash256(SHARED_SECRET, sha);
+    trace!("[ aes256 ] Secret key hashed.");
+    trace!("[ aes256 ] Key hash: {=[u8]:x}", keybuf);
+    let mut blocks: Vec<[u8; 16]> = Vec::new();
+    trace!("[ aes256 ] Chopping input into chunks of 16 bytes.");
+    let mut i = 0;
+    while i < buffer.len() {
+        trace!("[ aes256 ] Operating on chunk: {:?}", i);
+        let remaining = buffer.len() - i;
+        let current_chunk_size = core::cmp::min(16, remaining); // Handle last chunk
+        let mut chunk = [0u8; 16];
+        let data_slice = &buffer[i..i + current_chunk_size];
+        chunk[..data_slice.len()].copy_from_slice(data_slice);
+        trace!("[ aes256 ] Chunk: {=[u8]:x}", chunk);
+        blocks.push(chunk);
+        i += 16;
+    }
+    trace!("[ aes256 ] Chunk vector: {:x}", blocks);
+    for j in 0..blocks.len() {
+        match mode {
+            Mode::Encryption256 => {
+                trace!("[ aes256 ] Encrypting chunk: {:?}", j);
+                aes.process(&mut blocks[j], Mode::Decryption256, keybuf);
+                trace!("[ aes256 ] Encrypted chunk: {=[u8]:x}", blocks[j]);
+            }
+            Mode::Decryption256 => {
+                trace!("[ aes256 ] Decrypting chunk: {:?}", j);
+                aes.process(&mut blocks[j], Mode::Decryption256, keybuf);
+                trace!("[ aes256 ] Decrypted chunk: {=[u8]:x}", blocks[j]);
+            }
+            _ => (), // If user asks for other modes, this function is the identity and returns the plaintext.
+        }
+    }
+    let mut flattened = Vec::new();
+    trace!("[ aes256 ] Flattening chunk vector.");
+    for chunk in blocks {
+        flattened.extend_from_slice(&chunk);
+    }
+    trace!("[ aes256 ] Flattened vector: {=[u8]:x}", flattened);
+    return flattened;
 }
 
 fn your_program_here(_program_input: &Vec<u8>) -> Vec<u8> {
     return vec![0; 0];
 }
-
-// let mut aes = Aes::new(peripherals.AES);
-
-// // Hardcoded keys are obviously insecure. Consider this just an example!
-// let keytext = "SUp4SeCp@sSw0rd".as_bytes();
-// let plaintext = "message".as_bytes();
-
-// // create an array with aes128 key size
-// let mut keybuf = [0_u8; 32];
-// keybuf[..keytext.len()].copy_from_slice(keytext);
-
-// // create an array with aes block size
-// let mut block_buf = [0_u8; 16];
-// block_buf[..plaintext.len()].copy_from_slice(plaintext);
-
-// let mut block = block_buf.clone();
-// aes.process(&mut block, Mode::Encryption256, keybuf);
-// let hw_encrypted = block.clone();
-
-// aes.process(&mut block, Mode::Decryption256, keybuf);
-// let hw_decrypted = block;
